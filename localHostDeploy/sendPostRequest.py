fb7a9e2fee3b7edab5633f25b00f31f31627be60
Sasiwut Chaiyadecha <66057952+naenumtou@users.noreply.github.com>
2022-02-05 14:28:31 +0700

    Add files via upload

---
+++ localHostDeploy/sendPostRequest.py
@@ -0,0 +1,19 @@
+
+import requests
+
+URL = 'http://127.0.0.1:5000/prediction'
+X = {
+     'x1': 0.002287,
+     'x2': -0.02005,
+     'x3': 0.04493
+     }
+
+r = requests.post(URL, json = X)
+
+if r.status_code == 200:
+    print('Local host responded')
+    print(f'Prediction result: {r.text}')
+else:
+    print('Recheck deploy status')
+    
+    		
