b7b29b91f0f959009fb8c81533190b885d403b50
Sasiwut Chaiyadecha <66057952+naenumtou@users.noreply.github.com>
2022-02-05 14:27:12 +0700

    Add files via upload

---
+++ localHostDeploy/appPredict.py
@@ -0,0 +1,28 @@
+
+import pickle
+import numpy as np
+
+from flask import Flask, request
+
+app = Flask(__name__)
+model = pickle.load(open('model.pk', 'rb'))
+
+@app.route('/prediction', methods = ['POST', 'GET'])
+def prediction():
+    if request.method == 'GET':
+        return 'Resend with POST Method'
+    
+    elif request.method == 'POST':
+        X = request.get_json()
+        
+        x1 = X['x1']
+        x2 = X['x2']
+        x3 = X['x3']
+        XTest = np.array([[x1, x2, x3]])
+
+        predicted = model.predict(XTest)[0]
+        predicted = 1 / (1 + np.exp(-predicted))
+        return  str(predicted.round(4))
+
+if __name__ == '__main__':
+    app.run(debug = True)
